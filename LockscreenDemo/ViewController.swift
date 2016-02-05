686f58d6a8a70d90d9c5a03ba48a2829eb2bb0eb
René Köcher <shirk@bitspin.org>
2016-02-05 18:21:27 +0100

    Initial import

---
+++ LockscreenDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LockscreenDemo
+//
+//  Created by René Köcher on 05.02.16.
+//  Copyright © 2016 Rene Köcher - BitSpin Productions. All rights reserved.
+//  Sample Code for StackOverflow, use as you see fit
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
